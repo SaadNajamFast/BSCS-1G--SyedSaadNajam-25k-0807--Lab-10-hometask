7d60aa6eacd8a8517ba9cab6028c086c7bb57cdb
Syed Saad Najam <k250807@nu.edu.pk>
2025-11-19 23:58:06 +0500

    Update 3.c

--- 3.c
+++ 3.c
@@ -6,26 +6,23 @@ int main() {
     int vowels = 0, consonants = 0, digits = 0, spaces = 0, total = 0;
 
     printf("Enter a sentence: ");
-    fgets(text, sizeof(text), stdin);   // allows spaces
+    fgets(text, sizeof(text), stdin); 
 
     for (i = 0; text[i] != '\0'; i++) {
         char ch = text[i];
         total++;
 
-        // Vowel check
         if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||
             ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U') {
             vowels++;
         }
-        // Consonant check
+
         else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
             consonants++;
         }
-        // Digit check
         else if (ch >= '0' && ch <= '9') {
             digits++;
         }
-        // Space check
         else if (ch == ' ') {
             spaces++;
         }
