c8363ef2f425f391015f83b2863294ea833adc01
Syed Saad Najam <k250807@nu.edu.pk>
2025-11-19 23:56:56 +0500

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,32 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    char emails[4][50] = {
-        "user@gmail.com",
-        "john.doe@company.org",
-        "invalid-email",
-        "admin@university.edu"
-    };
+    char email[100];
+    int i, found = 0;
 
-    int i, j, found;
+    printf("Enter an email: ");
+    scanf("%s", email);
 
-    printf("Email Domain Extraction:\n\n");
-
-    for (i = 0; i < 4; i++) {
-        printf("Email: %s\t -> ", emails[i]);
-        found = 0;
-
-        for (j = 0; emails[i][j] != '\0'; j++) {
-            if (emails[i][j] == '@') {
-                printf("Domain: %s\n", &emails[i][j+1]);
-                found = 1;
-                break;
-            }
+    for (i = 0; email[i] != '\0'; i++) {
+        if (email[i] == '@') {
+            printf("Domain: %s\n", &email[i+1]);
+            found = 1;
+            break;
         }
+    }
 
-        if (!found) {
-            printf("Domain: Invalid email\n");
-        }
+    if (!found) {
+        printf("Domain: Invalid email\n");
     }
 
     return 0;
